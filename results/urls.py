8942ab4cf2349c0a96148160f64700297c1b0e1b
Lucas Marchesini Palma <lucasmarches@gmail.com>
2019-07-22 14:50:39 +0100

    Disabled updating section2

--- results/urls.py
+++ results/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('second', views.links_second, name='results_page_second'),
     path('third', views.links_third, name='results_page_third'),
     path('public_jobs/', views.PublicJobsView.as_view(), name='public_jobs'),
-    path('updated_section2', views.section2, name='public_jobs'),
+    #path('updated_section2', views.section2, name='public_jobs'),
     path('cade_cases/', views.CadeCasesView.as_view(), name='cade_cases')
 ]
